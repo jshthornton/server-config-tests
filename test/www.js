0706d9b556f7abc32f56cb04fee1d4441501db69
Joshua Thornton <joshua@techinasia.com>
2015-12-04 15:46:12 +0800

    Test non-www to www.

--- test/www.js
+++ test/www.js
@@ -5,6 +5,7 @@ var _ = require('lodash');
 
 describe('www', function() {
   if(config.www === true) {
+    // non-www -> www
     describe('non-www redirect', function() {
       context('When visiting non-www', function() {
         it('Should redirect non-www traffic to www', function(done) {
@@ -25,6 +26,25 @@ describe('www', function() {
       });
     });
   } else {
+    // www -> non-www
+    describe('www redirect', function() {
+      context('When visiting www', function() {
+        it('Should redirect www traffic to non-www', function(done) {
+          request('http://www.stackoverflow.com', function(error, response, body) {
+            expect(_.startsWith(response.request.host, 'www.')).to.equal(false);
+            done(error);
+          });
+        });
+      });
 
+      context('When visiting another subdomain', function() {
+        it('Should not redirect traffic to www', function() {
+          request('http://images.google.com', function(error, response, body) {
+            expect(_.startsWith(response.request.host, 'images.')).to.equal(true);
+            done(error);
+          });
+        });
+      });
+    });
   }
 });
